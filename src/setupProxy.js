b68d155c113e04764cddda20ad6bb24b9009954e
Daniele Guido <gui.daniele@gmail.com>
2022-07-05 16:40:33 +0200

    fix api and media proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,28 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const fs = require('fs')
+
+const target = process.env.REACT_APP_PROXY || 'http://localhost'
+const apiPath = process.env.REACT_APP_API_ROOT || '/api'
+
+fs.appendFile(
+  './setupProxy.log',
+  `${new Date().toISOString()} target=${target} apiPath=${apiPath}\n`,
+  (err) => console.error(err),
+)
+
+module.exports = function (app) {
+  app.use(
+    apiPath,
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    }),
+  )
+  app.use(
+    '/media',
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    }),
+  )
+}
