10e887ad91957624f4a81c5c11ebe8d166edefbb
Daniele Guido <gui.daniele@gmail.com>
2022-07-07 13:57:49 +0200

    add relevant info in collectionGrid

--- src/components/collection/CollectionCard.js
+++ src/components/collection/CollectionCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { truncate } from 'lodash';
-
+import { useTranslation } from 'react-i18next';
 import { LinkModal } from '../../hooks/modal';
 import { MediaRoute } from '../../constants';
+import DocumentDate from '../DocumentDate';
 
 import '../../styles/components/collection/CollectionCard.scss';
 
@@ -11,24 +12,39 @@ const TRUNCATE_OPTIONS = {
   separator: /[, ]/
 }
 
-const CollectionCard = ({ doc }) => (
-  <LinkModal
-    className     = "CollectionCard"
-    to            = {`../${MediaRoute.to}/${doc.slug}`}
-    useBackground
-  >
-    <div className="picture">
-      <div>
-        <img src={doc.data.resolutions?.thumbnail.url} alt={doc.title} />
+const CollectionCard = ({ doc }) => {
+
+  const { t } = useTranslation();
+
+  return (
+    <LinkModal
+      className     = "CollectionCard mb-2"
+      to            = {`../${MediaRoute.to}/${doc.slug}`}
+      useBackground
+    >
+      <div className="picture">
+        <div>
+          <img src={doc.data.resolutions?.thumbnail.url} alt={doc.title} />
+        </div>
       </div>
-    </div>
-    <div>
-      {truncate(doc.data.title, TRUNCATE_OPTIONS)}
-      {doc.data.index &&
-        <span> (p. {doc.data.index})</span>
-      }
-    </div>
-  </LinkModal>
-);
+      <div>
+        {truncate(doc.data.title, TRUNCATE_OPTIONS)}
+        {doc.data.index &&
+          <span> (p. {doc.data.index})</span>
+        }
+      </div>
+      <div>
+        <DocumentDate
+          doc={doc}
+          className="label"
+          separator="&middot;"
+        />
+        <span className="label text-gray-500">
+          {t(doc.data.type)}
+        </span>
+      </div>
+    </LinkModal>
+  )
+};
 
 export default CollectionCard;
